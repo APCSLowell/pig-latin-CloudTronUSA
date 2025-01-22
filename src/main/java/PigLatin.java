bf40801e0ff65c7c74cbdb63e2e5735634abb25d
CloudTron <96151994+CloudTronUSA@users.noreply.github.com>
2025-01-22 11:47:52 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -1,6 +1,8 @@
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PigLatin {
     
@@ -27,22 +29,29 @@ public class PigLatin {
 	        System.out.println(pigLatin(lines[i]));
 	    }
     }
+	
     public int findFirstVowel(String sWord) {
         //precondition: sWord is a valid String of length greater than 0.
         //postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
-	    // your code goes here
+    	ArrayList<String> t = new ArrayList<String>(Arrays.asList("a", "e", "i", "o", "u"));
+    	for (int i=0;i<sWord.length();i++) {
+            if (t.stream().anyMatch(sWord.substring(i,i+1)::equals)) {return i;}
+        }
         return -1;
     }
 
     public String pigLatin(String sWord) {
         //precondition: sWord is a valid String of length greater than 0
         //postcondition: returns the pig latin equivalent of sWord
-        // more code should go here
-	    if(findFirstVowel(sWord) == -1) {
+	    if(findFirstVowel(sWord) == -1)    // all consonants
 		    return sWord + "ay";
-	    }
-	    else {
-		return "ERROR!";
-	    }
+        if(sWord.length()>1 && sWord.substring(0,2).equals("qu"))    // starts with qu
+		    return sWord.substring(2) + "quay";
+        if(findFirstVowel(sWord) == 0)    // starts with vowel
+		    return sWord + "way";
+        else {    // starts with consonats
+            int v = findFirstVowel(sWord);
+            return sWord.substring(v) + sWord.substring(0, v) + "ay";
+        }
     }
-}//end PigLatin class
+} //end PigLatin class
